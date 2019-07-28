43a596d8ca3bf33cdbca450c35b695a90ff5a23b
Michael Barz <mb@barz.de>
2019-07-28 18:41:13 +0200

    turned sample app into simple weight sensor data visualization; refactored to other namespace

---
+++ app/src/main/java/de/barz/bbq_gas_meter/SerialListener.java
@@ -0,0 +1,8 @@
+package de.barz.bbq_gas_meter;
+
+interface SerialListener {
+    void onSerialConnect      ();
+    void onSerialConnectError (Exception e);
+    void onSerialRead         (byte[] data);
+    void onSerialIoError      (Exception e);
+}
